ae3a2a2b5e5d98ef8b470ea1057ead4136a9ceb9
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-10-05 15:32:50 +0200

    Finalized iteration3

--- js/index.js
+++ js/index.js
@@ -14,3 +14,40 @@ if (hacker1.length > hacker2.length) {
 }
 
 // Iteration 3: Loops
+//3.1
+let result = "";
+for (let i = 0; i < hacker1.length; i++) {
+    result = result + hacker1[i].toUpperCase() + " ";
+}
+console.log(result);
+
+//3.2
+let reverse = "";
+for (let j = hacker2.length; j > 0; j--) {
+    reverse = reverse + hacker2[j-1];
+}
+console.log(reverse);
+
+//3.3
+let iteration = 0;
+if (hacker1.length < hacker2.length) {
+    iteration = hacker1.length;
+} else if (hacker1.length > hacker2.length) {
+    iteration = hacker2.length;
+} else {
+    iteration = hacker1.length;
+}
+let names = "";
+for (let k = 0; k < iteration; k++) {
+    if (hacker1[k] == hacker2[k]) {
+        names = "What?! You both have the same name?";
+    } else if (hacker1[k] > hacker2[k]) {
+        names = "Yo, the navigator goes first definitely.";
+        break;
+    } else if (hacker1[k] < hacker2[k]) {
+        names = "The driver's name goes first.";
+        break;
+    }
+}
+console.log(names);
+
