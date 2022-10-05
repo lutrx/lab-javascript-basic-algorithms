6a7189a15686b2404414d5666022d2ef96042d7b
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-10-05 12:38:02 +0200

    Finalized iteration1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,9 @@
+//console.log("I'm ready!");
 // Iteration 1: Names and Input
-//
+const hacker1 = "Lisa";
+console.log(`The driver's name is ${hacker1}.`);
+const hacker2 = "Enrico";
+console.log(`The navigator's name is ${hacker2}.`);
 // Iteration 2: Conditionals
 
 
